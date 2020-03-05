f57a7e2b6359e4bb0060fd7dc16e906cfd55ea03
Ethan <zyyhaw@gmail.com>
2020-03-05 01:34:02 +0000

    base layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      Code By <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Ethan</a>  <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+  </div>
+</footer>
