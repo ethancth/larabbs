2797924c901b88057bca18526943c5cbea033b44
Ethan <zyyhaw@gmail.com>
2020-03-05 23:18:20 +0000

    add notification after Email Verify

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class EmailVerified
+{
+    public function handle(Verified $event)
+    {
+
+        session()->flash('success', 'Email Verify! ^_^');
+    }
+}
