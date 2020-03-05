601adb405d7a322512017434e6da0c9b4df533cf
Ethan <zyyhaw@gmail.com>
2020-03-05 22:48:05 +0000

    Register with Captcha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title', 'LaraBBS') - Demo - VMware & Docker</title>
+  <title>@yield('title', 'LaraSSP') - Demo - SSP & Docker</title>
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
